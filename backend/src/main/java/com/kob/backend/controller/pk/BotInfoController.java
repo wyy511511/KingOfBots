506d35797846051fab28524924124f89cd64c461
wyy511511 <wuyaoyao0309@163.com>
2023-01-08 16:42:50 -0700

    backend data to front end

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,11 +3,21 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 @RestController // similar to REST, different with frontend and backend together
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("getbotinfo/")
-    public String getBotInfo(){
-        return "hhhhh";
+//    public String getBotInfo(){
+//        return "hhhhh";
+//    }
+    public Map<String, String> getBotInfo(){
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "tiger");
+        bot1.put("rating", "1400");
+        return bot1;
     }
 }
--- web/src/App.vue
+++ web/src/App.vue
@@ -1,16 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div> Bot name:{{ bot_name}} </div>
+    <div> Bot rating: {{ bot_rating}} </div>
+
+  </div>
+    <router-view> </router-view>
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from 'vue';
+import $ from 'jquery';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  setup: () => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
+
+    $.ajax({
+      url: "http://127.0.0.1:8080/pk/getbotinfo/",
+      type: "get",
+      success: resp => {
+        console.log(resp);
+        bot_name.value = resp.name;
+        bot_rating.value = resp.rating;
+      }
+    });
+
+    return {
+      bot_name,
+      bot_rating
+    } 
+
   }
+
 }
 </script>
 
