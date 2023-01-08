c98212dbc20d3ffa6b7603816722fcddd8cac89b
wyy511511 <wuyaoyao0309@163.com>
2023-01-07 19:43:29 -0700

    http://127.0.0.1:8080/pk/index/

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -1,5 +1,14 @@
 package com.kob.backend.controller.pk;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
+@RequestMapping("/pk/")
 public class IndexController {
+    //create page: resources/template/ new directory be named pk
+    @RequestMapping("index/")
+    public String index() {
+        return "pk/index.html";
+    }
+
+
 }
