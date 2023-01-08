64c635bec1e0272319bd21775871fe85a41ccba3
wyy511511 <wuyaoyao0309@163.com>
2023-01-08 14:53:52 -0700

    divide bakcend and frontend

---
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -0,0 +1,7 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // similar to REST, different with frontend and backend together
+public class BotInfoController {
+}
