a5cd410e6755fa797ebced9535f7e6ed8730dcab
wyy511511 <wuyaoyao0309@163.com>
2023-01-09 21:08:41 -0700

    navigation bar, map of pk and obstacles

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,54 +1,23 @@
 <template>
-
-  <div>
-    <div> Bot name:{{ bot_name}} </div>
-    <div> Bot rating: {{ bot_rating}} </div>
-
-  </div>
-    <router-view> </router-view>
+  <NavBar />
+  <router-view></router-view>
 </template>
-
-
 
 <script>
 import NavBar from './components/NavBar.vue'
-import {ref} from 'vue';
-import $ from 'jquery';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
 
 export default {
-  name: 'App',
-
-  setup: () => {
-    let bot_name = ref("");
-    let bot_rating = ref("");
-
-    $.ajax({
-      url: "http://127.0.0.1:8080/pk/getbotinfo/",
-      type: "get",
-      success: resp => {
-        console.log(resp);
-        bot_name.value = resp.name;
-        bot_rating.value = resp.rating;
-      }
-    });
-
-    return {
-      bot_name,
-      bot_rating
-    } 
-
+  components: {
+    NavBar
   }
-
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-image: url("@/assets/images/background.png");
+  background-size: cover;
 }
 </style>
